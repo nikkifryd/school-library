178f891a37e96dc6a052fcc030115f253f37f846
Nikki Frey <nico.bastian.frey@gmail.com>
2026-02-27 21:53:35 +0100

    rebase to main

--- apps/api/listen/requestParser.js
+++ apps/api/listen/requestParser.js
@@ -1,19 +1,47 @@
 import * as database from '../process/process.js'
 
+/**Notation:
+ * -'' on top
+ * -clear next
+ * -keys last
+ * -Only one at one level
+ */
 const routes = {
     'GET': {
-        'books': (res, id) => id ? getBook(res,id) : getBooks(res,id),
-        'students': (res, id) => id ? getStudent(res,id): getStudents(res,id)
+        'books': {
+            '' : {},
+            '$id': {
+                '': {},
+                'author': {}
+            }
+        },
+        'students': {
+
+        },
+        'test': {
+            'lurch': {
+                '': (res, params) => console.log("Desch is a lurch, ganz klar sigg i des"),
+                'baby':(res,params) => console.log('BABY LURCH AWW')
+            },
+            '$tier': (res, params) => console.log("Es ist ein: " + params.tier),
+
+            '$gattung': {
+                '':(res, params) => console.log('Nur die Gattung '+ params.gattung),
+                '$tier': {
+                    '':(res, params) => console.log('Es ist ein '+params.tier+' der Gattung '+params.gattung),
+                    'pfote': (res, params) => console.log('Es ist eine '+params.tier+'-Pfote aus der Gattung '+params.gattung),
+                    '$koerperteil': (res, params) => console.log('Es ist eins '+params.tier+'-'+params.koerperteil+' aus der Gattung '+params.gattung)
+                },
+            }
+        }
     }
 };
 
- /* Decipher received client request
- * -find out method
- * -pass on body
+ /** Parse client request
  * @param {http.IncomingMessage} req Client request
  * @param {http.ServerResponse} res Server response
  */
-export function handleRequest (req,res){
+export function handleRequest (req,res) {
     //filter(Boolean) gets rid of empty entries
     let endpoints = req.url.split('/').filter(Boolean);
     let method = req.method;
@@ -24,8 +52,46 @@ export function handleRequest (req,res){
         return;
     }
 
-    if (routes[method][endpoints[1]]) {
-        routes[method][endpoints[1]](res, endpoints[2]);
+    parseRoute(routes[method], endpoints.slice(1), {}, req, res);
+}
+
+/**
+ * Parses the requested URL along the above defined routes recursively
+ * @param {[string]} endpoints Endpoints contained in the URL
+ * @param {object} currentRoute The point in the route currently at
+ * @param {object} parameter The parameters passed in the keys
+ * @param {http.IncomingMessage} req IncomingMessage passed down to the function at the route
+ * @param {http.ServerResponse} res ServerResponse passed down to the function at the route
+ * @returns {Function} The function called at the end of the route
+ */
+function parseRoute (endpoints, currentRoute, parameter, req, res) {
+    if(endpoints.length === 0) {
+        if (typeof currentRoute === 'function') {
+            return currentRoute(parameter, req, res);
+        }
+
+        if(typeof currentRoute[''] === 'function') {
+            return currentRoute[''](parameter, req, res);
+        }
+    }
+
+
+    let nextEndpoint = endpoints[0];
+    let params = parameter;
+
+    if(nextEndpoint in currentRoute) {
+        return parseRoute(endpoints.slice(1), currentRoute[nextEndpoint], params, req, res);
+    }
+
+    for(let routeKey in currentRoute) {
+        if(routeKey.startsWith('$')) {
+            let paramName = routeKey.slice(1);
+            params[paramName] = nextEndpoint;
+
+            let keyValue = parseRoute(endpoints.slice(1), currentRoute[routeKey], params, req, res);
+            if(typeof keyValue === 'function') 
+                return keyValue(params, req, res);
+        }
     }
 }
 
