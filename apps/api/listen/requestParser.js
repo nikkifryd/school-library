62a18957a06f50d98a4914e33d77d02c7eeeddd8
Nikki Frey <nico.bastian.frey@gmail.com>
2026-02-27 13:07:43 +0100

    URLparser almost done

--- apps/api/listen/requestParser.js
+++ apps/api/listen/requestParser.js
@@ -1,16 +1,17 @@
-import { parse } from 'path';
 import * as database from '../process/process.js'
 
 const routes = {
     'GET': {
         'books': {
-            null : getBooks(res),
+            null : {},
             '$id': {
-                null: getBook(res, parameter),
-                'author': (res, parameter) => console.log('Autor des Buches mit der ID:'+parameter.id)
+                null: {},
+                'author': {}
             }
         },
-        'students': (res, params) => id ? getStudent(res, id) : getStudents(res, id),
+        'students': {
+
+        },
         'test': {
             // GET /api/test/lurch => "lurch"
             'lurch': (res, params) => "lurch",
@@ -21,7 +22,7 @@ const routes = {
 
             '$gattung': {
                 // GET /api/test/Canine/Wolf => "Gattung: Canine Tier: Wolf"
-                '$tier': (res, params) => console.log('Gattung: ${params.gattung} Tier: ${params.tier}')
+                '$tier': (res, params) => console.log('Es ist ein '+params.tier+' der Gattung '+params.gattung)
             }
         }
     }
@@ -44,8 +45,6 @@ export function handleRequest (req,res){
         return;
     }
 
-    endpoints = endpoints.slice(1);
-
     parseRoute(routes[method], endpoints.slice(1), {});
 }
 
@@ -56,20 +55,31 @@ export function handleRequest (req,res){
  * @param {object} parameter 
  */
 function parseRoute (currentRoute, endpoints, parameter) {
+    //tester delete
+    console.log(endpoints+'('+parameter+'):\n');
+    console.log(currentRoute);
+
     let nextEndpoint = endpoints[0];
     let params = parameter;
 
+    if(endpoints.length === 0) {
+        if (typeof currentRoute === 'function') {
+            currentRoute(undefined,parameter);
+        }
+        return;
+    }
+
     if(nextEndpoint in currentRoute) {
         parseRoute(currentRoute[nextEndpoint], endpoints.slice(1), params);
+        return;
     }
-    else {
-        for(routeKey in currentRoute) {
-            if(routeKey.startsWith('$')) {
-                let paramName = routeKey.slice(1);
 
-                params[paramName] = nextEndpoint;
-                
-            }
+    for(let routeKey in currentRoute) {
+        if(routeKey.startsWith('$')) {
+            let paramName = routeKey.slice(1);
+
+            params[paramName] = nextEndpoint;
+            parseRoute(currentRoute[routeKey], endpoints.slice(1),params);
         }
     }
 }
