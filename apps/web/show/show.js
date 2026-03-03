99eab8eb01ccedc9c92adbc63360e55f5c6ff8b3
Nikki Frey <nico.bastian.frey@gmail.com>
2026-03-03 14:49:29 +0100

    #29: Search Book by id on start page

--- apps/web/show/show.js
+++ apps/web/show/show.js
@@ -1,10 +1,24 @@
 const bookTable = document.getElementById("bookTable");
-var books;
+const searchBook = document.getElementById("searchBook");
+
 
 fetch('http://127.0.0.1:8000/api/books').then((response) => response.json())
 .then(fillBookTable);
 
+searchBook.addEventListener('input', (event) => {
+    fetch('http://127.0.0.1:8000/api/books/'+searchBook.value).then((response) => response.json())
+    .then(fillSearchResult);
+})
+
+function emptyBookTable () {
+    for(let row of bookTable.children) {
+        if(row.tagName === 'TR')
+            bookTable.removeChild(row);
+    }
+}
+
 function fillBookTable(books) {
+    emptyBookTable();
 
     for (let book of books) {
         let row = document.createElement('tr');
@@ -18,20 +32,12 @@ function fillBookTable(books) {
         bookTable.appendChild(row);
     };
 }
-/*var books = [
-    {
-        "id": 1,
-        "title": "Pipikakaland",
-        "author": ''
-    },
-    {
-        "id": 2,
-        "title": "Gutenachtgeschichten",
-        "author": ''
-    },
-    {
-        "id": 3,
-        "title": "Gregs Tagebuch",
-        "author": ''
-    }
-];*/
+
+function fillSearchResult(books) {
+    
+    console.log(bookTable);
+    emptyBookTable();    
+
+    fillBookTable(books);
+    console.log(bookTable);
+}
